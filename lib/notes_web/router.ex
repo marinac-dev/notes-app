566c79ee8d7871b7683848d36a4b7efa77d9607b
Marinac <nikola@regex-sh.com>
2021-09-06 00:43:45 +0200

    Update router

--- lib/notes_web/router.ex
+++ lib/notes_web/router.ex
@@ -1,5 +1,6 @@
 defmodule NotesWeb.Router do
   use NotesWeb, :router
+  import NotesWeb.Plugin.Auth
 
   pipeline :api do
     plug :accepts, ["json"]
@@ -7,21 +8,42 @@ defmodule NotesWeb.Router do
 
   scope "/api", NotesWeb do
     pipe_through :api
+
+    # Api versioning
+    scope "/v1" do
+      # Public scope for auth
+      scope "/auth" do
+        post "/sign-in", SessionController, :sign_in
+        post "/sign-up", SessionController, :sign_up
+      end
+
+      # Private scope | requires authentication
+      scope "/" do
+        # Disable auth for test env
+        if Mix.env() != :test do
+          pipe_through :require_auth
+        end
+
+        resources "/users", UserController, except: [:new, :create, :edit, :index] do
+          # Notes shared with user
+          get "/shares", ShareController, :shares
+          # Notes shared by user
+          get "/shared", ShareController, :index
+          post "/notes/:note_id/share", ShareController, :create
+
+          resources "/notes", NoteController, except: [:new, :edit] do
+            resources "/files", FileController, except: [:new, :edit]
+          end
+        end
+      end
+    end
   end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  # Live Dashboard
+  import Phoenix.LiveDashboard.Router
 
-    scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-      live_dashboard "/dashboard", metrics: NotesWeb.Telemetry
-    end
+  scope "/" do
+    pipe_through [:fetch_session, :protect_from_forgery]
+    live_dashboard "/dashboard", metrics: NotesWeb.Telemetry
   end
 end
