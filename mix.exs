9075861af6e75c6badcb60b53e964419738fb090
Marinac <nikola@regex-sh.com>
2021-09-05 13:39:13 +0200

    Add bcrypt

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Notes.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:bcrypt_elixir, "~> 2.3"}
     ]
   end
 
