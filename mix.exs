ff3fff2449f4091cae172fba2322bb24424a145a
Marinac <nikola@regex-sh.com>
2021-09-05 13:42:18 +0200

    Add coverall and credo

--- mix.exs
+++ mix.exs
@@ -43,7 +43,9 @@ defmodule Notes.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:bcrypt_elixir, "~> 2.3"}
+      {:bcrypt_elixir, "~> 2.3"},
+      {:excoveralls, "~> 0.14.2"},
+      {:credo, "~> 1.5"}
     ]
   end
 
