f4523ef74974cbf37008818687ac460ed22e7410
Marinac <nikola@regex-sh.com>
2021-09-05 13:40:08 +0200

    Update user with input validation and pass hash

--- lib/notes/accounts/user.ex
+++ lib/notes/accounts/user.ex
@@ -2,9 +2,11 @@ defmodule Notes.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @derive {Inspect, except: [:password]}
   schema "users" do
-    field :password_hash, :string
     field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
 
     timestamps()
   end
@@ -12,8 +14,55 @@ defmodule Notes.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:username, :password_hash])
-    |> validate_required([:username, :password_hash])
+    |> cast(attrs, [:username, :password])
+    |> validate_username()
+    |> validate_password()
+  end
+
+  defp validate_username(changeset) do
+    changeset
+    |> validate_required([:username])
+    |> validate_length(:username, min: 2, max: 32)
+    |> validate_format(:username, ~r/[a-zA-Z0-9]+$/,
+      message: "Username can contain only letters and numbers"
+    )
+    |> unsafe_validate_unique(:username, Notes.Repo)
     |> unique_constraint(:username)
+  end
+
+  defp validate_password(changeset) do
+    changeset
+    |> validate_required([:password])
+    |> validate_length(:password, min: 12, max: 80)
+    |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
+    |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
+    |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/,
+      message: "at least one digit or punctuation character"
+    )
+    |> prepare_changes(&hash_password/1)
+  end
+
+  defp hash_password(changeset) do
+    password = get_change(changeset, :password)
+
+    changeset
+    |> put_change(:password_hash, Bcrypt.hash_pwd_salt(password))
+    |> delete_change(:password)
+  end
+
+  @doc """
+  Verifies the password.
+
+  If there is no user or the user doesn't have a password, we call
+  `Bcrypt.no_user_verify/0` to avoid timing attacks.
+  """
+  def valid_password?(%__MODULE__{password_hash: password_hash}, password)
+      when is_binary(password_hash) and byte_size(password) > 0 do
+    Bcrypt.verify_pass(password, password_hash)
+  end
+
+  def valid_password?(_, _) do
+    Bcrypt.no_user_verify()
+    false
   end
 end
