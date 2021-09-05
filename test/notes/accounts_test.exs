6c8e3317ff9dce9a41749038d4dba9a9d30a382b
Marinac <nikola@regex-sh.com>
2021-09-05 17:49:09 +0200

    Remove notes and files tests

--- test/notes/accounts_test.exs
+++ test/notes/accounts_test.exs
@@ -7,7 +7,10 @@ defmodule Notes.AccountsTest do
     alias Notes.Accounts.User
 
     @valid_attrs %{password_hash: "some password_hash", username: "some username"}
-    @update_attrs %{password_hash: "some updated password_hash", username: "some updated username"}
+    @update_attrs %{
+      password_hash: "some updated password_hash",
+      username: "some updated username"
+    }
     @invalid_attrs %{password_hash: nil, username: nil}
 
     def user_fixture(attrs \\ %{}) do
@@ -61,128 +64,6 @@ defmodule Notes.AccountsTest do
     test "change_user/1 returns a user changeset" do
       user = user_fixture()
       assert %Ecto.Changeset{} = Accounts.change_user(user)
-    end
-  end
-
-  describe "notes" do
-    alias Notes.Accounts.Note
-
-    @valid_attrs %{content: "some content"}
-    @update_attrs %{content: "some updated content"}
-    @invalid_attrs %{content: nil}
-
-    def note_fixture(attrs \\ %{}) do
-      {:ok, note} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Accounts.create_note()
-
-      note
-    end
-
-    test "list_notes/0 returns all notes" do
-      note = note_fixture()
-      assert Accounts.list_notes() == [note]
-    end
-
-    test "get_note!/1 returns the note with given id" do
-      note = note_fixture()
-      assert Accounts.get_note!(note.id) == note
-    end
-
-    test "create_note/1 with valid data creates a note" do
-      assert {:ok, %Note{} = note} = Accounts.create_note(@valid_attrs)
-      assert note.content == "some content"
-    end
-
-    test "create_note/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Accounts.create_note(@invalid_attrs)
-    end
-
-    test "update_note/2 with valid data updates the note" do
-      note = note_fixture()
-      assert {:ok, %Note{} = note} = Accounts.update_note(note, @update_attrs)
-      assert note.content == "some updated content"
-    end
-
-    test "update_note/2 with invalid data returns error changeset" do
-      note = note_fixture()
-      assert {:error, %Ecto.Changeset{}} = Accounts.update_note(note, @invalid_attrs)
-      assert note == Accounts.get_note!(note.id)
-    end
-
-    test "delete_note/1 deletes the note" do
-      note = note_fixture()
-      assert {:ok, %Note{}} = Accounts.delete_note(note)
-      assert_raise Ecto.NoResultsError, fn -> Accounts.get_note!(note.id) end
-    end
-
-    test "change_note/1 returns a note changeset" do
-      note = note_fixture()
-      assert %Ecto.Changeset{} = Accounts.change_note(note)
-    end
-  end
-
-  describe "files" do
-    alias Notes.Accounts.File
-
-    @valid_attrs %{extension: "some extension", name: "some name", path: "some path"}
-    @update_attrs %{extension: "some updated extension", name: "some updated name", path: "some updated path"}
-    @invalid_attrs %{extension: nil, name: nil, path: nil}
-
-    def file_fixture(attrs \\ %{}) do
-      {:ok, file} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Accounts.create_file()
-
-      file
-    end
-
-    test "list_files/0 returns all files" do
-      file = file_fixture()
-      assert Accounts.list_files() == [file]
-    end
-
-    test "get_file!/1 returns the file with given id" do
-      file = file_fixture()
-      assert Accounts.get_file!(file.id) == file
-    end
-
-    test "create_file/1 with valid data creates a file" do
-      assert {:ok, %File{} = file} = Accounts.create_file(@valid_attrs)
-      assert file.extension == "some extension"
-      assert file.name == "some name"
-      assert file.path == "some path"
-    end
-
-    test "create_file/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Accounts.create_file(@invalid_attrs)
-    end
-
-    test "update_file/2 with valid data updates the file" do
-      file = file_fixture()
-      assert {:ok, %File{} = file} = Accounts.update_file(file, @update_attrs)
-      assert file.extension == "some updated extension"
-      assert file.name == "some updated name"
-      assert file.path == "some updated path"
-    end
-
-    test "update_file/2 with invalid data returns error changeset" do
-      file = file_fixture()
-      assert {:error, %Ecto.Changeset{}} = Accounts.update_file(file, @invalid_attrs)
-      assert file == Accounts.get_file!(file.id)
-    end
-
-    test "delete_file/1 deletes the file" do
-      file = file_fixture()
-      assert {:ok, %File{}} = Accounts.delete_file(file)
-      assert_raise Ecto.NoResultsError, fn -> Accounts.get_file!(file.id) end
-    end
-
-    test "change_file/1 returns a file changeset" do
-      file = file_fixture()
-      assert %Ecto.Changeset{} = Accounts.change_file(file)
     end
   end
 end
