39d2ca92bd8bb738dbb573440f02255eb93a1071
Marinac <nikola@regex-sh.com>
2021-09-05 15:14:43 +0200

    relate notes to user

--- lib/notes/accounts/user.ex
+++ lib/notes/accounts/user.ex
@@ -7,6 +7,7 @@ defmodule Notes.Accounts.User do
     field :username, :string
     field :password, :string, virtual: true
     field :password_hash, :string
+    has_many :notes, Notes.Accounts.Note
 
     timestamps()
   end
