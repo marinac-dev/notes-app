9b905b5d58b532131ecae6dd89f73d7ceec65522
Marinac <nikola@regex-sh.com>
2021-09-06 00:50:02 +0200

    Update note view

--- lib/notes_web/views/note_view.ex
+++ lib/notes_web/views/note_view.ex
@@ -1,6 +1,6 @@
 defmodule NotesWeb.NoteView do
   use NotesWeb, :view
-  alias NotesWeb.NoteView
+  alias NotesWeb.{NoteView, FileView}
 
   def render("index.json", %{notes: notes}) do
     %{data: render_many(notes, NoteView, "note.json")}
@@ -11,7 +11,11 @@ defmodule NotesWeb.NoteView do
   end
 
   def render("note.json", %{note: note}) do
-    %{id: note.id,
-      content: note.content}
+    %{
+      id: note.id,
+      type: "note",
+      content: note.content,
+      files: FileView.render("index.json", files: note.files)
+    }
   end
 end
